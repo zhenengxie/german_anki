1edf197cfe0e7b0d2bbd37d334fe8ff0e31189e8
Zheneng Xie <zhenengxie97@gmail.com>
2019-11-19 10:21:36 +0000

    added auto audio generation for manual input of fields

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from anki.hooks import addHook
 import hashlib
 from google.cloud import texttospeech
 from german_anki.conjugator import conjugate_verb, declenations_noun, declenations_adj
+from bs4 import BeautifulSoup
 
 CLIENT = texttospeech.TextToSpeechClient()
 VOICE = texttospeech.types.VoiceSelectionParams(
@@ -12,7 +13,10 @@ VOICE = texttospeech.types.VoiceSelectionParams(
 AUDIO_CONFIG = texttospeech.types.AudioConfig(
     audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-def tts(text):
+def tts(text, prefix = None):
+    text = BeautifulSoup(text, "lxml").text # parsing out all html tags
+    if prefix is not None:
+        text = prefix + ' ' + text
     filename = "german-gtts-{0}.mp3".format(hashlib.md5(text.encode('utf-8')).hexdigest())
 
     try:
@@ -26,7 +30,35 @@ def tts(text):
 
     return "[sound:{0}]".format(filename)
 
-def format_noun(gender, word, case, raw):
+def add_sound_to_field(note, field, prefix=None):
+    if note[field]:
+        note[field + ' Sound'] = tts(note[field], prefix = prefix)
+
+def add_sound(note, note_type):
+    if note_type == 'noun':
+        for field in ['Nominative Singular', 'Nominative Plural', 'Genitive Singular']:
+            add_sound_to_field(note, field)
+    if note_type == 'adj':
+        for field in ['Lemma', 'Predicative', 'Comparative', 'Superlative']:
+            add_sound_to_field(note, field)
+    if note_type == 'verb':
+        for prefix, field in [
+                ('', 'Infinitive'),
+                ('ich ', 'Present 1'),
+                ('du ', 'Present 2 Sing'),
+                ('es ', 'Present 3'),
+                ('wir ', 'Present 1 3 Plural 2 Formal'),
+                ('ihr ', 'Present 2 Plural'),
+                ('', 'Aux Past Participle'),
+                ('', 'Imperative Singular'),
+                ('ich ', 'Conjunctive II 1'),
+                ('ich ', 'Imperfect 1')
+                ]:
+            add_sound_to_field(note, field, prefix)
+    if note_type == 'general':
+        add_sound_to_field(note, 'German')
+
+def format_noun(gender, word, case):
     articles = {
             ('m', 'nom'): 'der',
             ('f', 'nom'): 'die',
@@ -39,68 +71,56 @@ def format_noun(gender, word, case, raw):
             }
 
     rawtext = articles[(gender, case)] + ' ' + word
-    if raw:
-        return rawtext
-    else:
-        return """<span class="{0}">{1}</span>""".format(gender, rawtext)
+    return """<span class="{0}">{1}</span>""".format(gender, rawtext)
 
 def comma_join(lst):
     return ", ".join(lst)
 
+
+GERMAN_NOUN_NAME = "German Nouns"
+GERMAN_ADJV_NAME = "German Adjectives"
+GERMAN_VERB_NAME = "German Verbs"
+GERMAN_GENR_NAME = "German General"
+
 def onFocusLost(flag, note, fidx):
     from aqt import mw
+
+    note_type = note.model()['name']
+    field_indices = {card: i for i, card in enumerate(mw.col.models.fieldNames(note.model()))}
     
-    if note.model()['name'] == 'AUTO German Nouns':
-        field_indices = {card: i for i, card in enumerate(mw.col.models.fieldNames(note.model()))}
+    if note_type == GERMAN_NOUN_NAME:
         if field_indices['Auto'] == fidx and not note['Manual Override']:
             nouns = [noun.strip() for noun in note['Auto'].strip().split(',')]
             decls = [declenations_noun(noun) for noun in nouns]
             decls = [decl for decl in decls if decl]
 
             nom_sing = []
-            nom_sing_raw = []
             for decl in decls:
                 if 'Nominative Singular' in decl:
-                    nom_sing.append(format_noun(decl['Gender'],
-                        decl['Nominative Singular'], 'nom', False))
-                    nom_sing_raw.append(format_noun(decl['Gender'],
-                        decl['Nominative Singular'], 'nom', True))
+                    nom_sing.append(format_noun(decl['Gender'], decl['Nominative Singular'], 'nom'))
             nom_sing = ", ".join(nom_sing)
-            nom_sing_raw = ", ".join(nom_sing_raw)
             note['Nominative Singular'] = nom_sing
-            if nom_sing_raw:
-                note['Nominative Singular Sound'] = tts(nom_sing_raw)
 
             nom_pl = []
             nom_pl_raw = []
             for decl in decls:
                 if 'Nominative Plural' in decl:
-                    nom_pl.append(format_noun('pl',
-                        decl['Nominative Plural'], 'nom', False))
-                    nom_pl_raw.append(format_noun('pl',
-                        decl['Nominative Plural'], 'nom', True))
+                    nom_pl.append(format_noun('pl', decl['Nominative Plural'], 'nom'))
             nom_pl = ", ".join(nom_pl)
-            nom_pl_raw = ", ".join(nom_pl_raw)
             note['Nominative Plural'] = nom_pl
-            if nom_pl_raw:
-                note['Nominative Plural Sound'] = tts(nom_pl_raw)
 
             if any(decl['Gender'] in ['m', 'n'] for decl in decls):
                 gen_forms = []
-                gen_forms_raw = []
                 for decl in decls:
                     if 'Genitive Singular Forms' in decl:
-                        gen_forms += [format_noun(decl['Gender'], form, 'gen', False)
-                                for form in decl['Genitive Singular Forms']]
-                        gen_forms_raw += [format_noun(decl['Gender'], form, 'gen', True)
+                        gen_forms += [format_noun(decl['Gender'], form, 'gen')
                                 for form in decl['Genitive Singular Forms']]
                 note['Genitive Singular'] = ", ".join(gen_forms)
-                note['Genitive Singular Sound'] = tts(", ".join(gen_forms_raw))
+            else:
+                note['Genitive Singular'] = ""
 
-            return True
 
-    if note.model()['name'] == 'AUTO German Adjectives':
-        field_indices = {card: i for i, card in enumerate(mw.col.models.fieldNames(note.model()))}
+    if note_type == GERMAN_ADJV_NAME:
         if field_indices['Auto'] == fidx and not note['Manual Override']:
             adjs = [adj.strip() for adj in note['Auto'].split(',')]
             decls = [declenations_adj(adj) for adj in adjs]
@@ -125,12 +145,8 @@ def onFocusLost(flag, note, fidx):
                 if values:
                     values = comma_join(values)
                     note[field] = values
-                    if field != 'Stem':
-                        note[field + ' Sound'] = tts(values)
-            return True
 
-    if note.model()['name'] == 'AUTO German Verbs':
-        field_indices = {card: i for i, card in enumerate(mw.col.models.fieldNames(note.model()))}
+    if note_type == GERMAN_VERB_NAME:
         if field_indices['Auto'] == fidx and not note['Manual Override']:
             inf = note['Auto'].strip()
 
@@ -151,20 +167,16 @@ def onFocusLost(flag, note, fidx):
 
                 note['Imperative Singular'] = ', '.join(conj['Imperative Singular'])
 
-                for prefix, field in [
-                        ('', 'Infinitive'),
-                        ('ich ', 'Present 1'),
-                        ('du ', 'Present 2 Sing'),
-                        ('es ', 'Present 3'),
-                        ('wir ', 'Present 1 3 Plural 2 Formal'),
-                        ('ihr ', 'Present 2 Plural'),
-                        ('', 'Aux Past Participle'),
-                        ('', 'Imperative Singular'),
-                        ('ich ', 'Conjunctive II 1'),
-                        ('ich ', 'Imperfect 1')
-                        ]:
-                    note[field + ' Sound'] = tts(prefix + note[field])
-
+    if any(field in field_indices and field_indices[field] == fidx for field in ['Auto', 'German', 'Manual Override']):
+        if 'Manual Override' in note and (not note['Manual Override'] or note['Manual Override'] == 'a'):
+            if note_type == GERMAN_NOUN_NAME:
+                add_sound(note, 'noun')
+            if note_type == GERMAN_VERB_NAME:
+                add_sound(note, 'verb')
+            if note_type == GERMAN_ADJV_NAME:
+                add_sound(note, 'adj')
+            if note_type == GERMAN_GENR_NAME:
+                add_sound(note, 'general')
             return True
 
     return False
