bddbe3248eca1079ff5d8132454f54fa2bddc3b3
Zheneng Xie <zhenengxie97@gmail.com>
2020-05-07 22:40:10 +0100

    added verbix scraper

---
+++ verbix_online.py
@@ -0,0 +1,79 @@
+""" Scraper for Verbix.com """
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+
+OPTIONS = Options()
+OPTIONS.headless = True
+DRIVER = webdriver.Firefox(options=OPTIONS, executable_path="./geckodriver")
+
+URL = """http://verbix.com/webverbix/German/{0}.html"""
+
+def scrape_verbix(verb):
+    """ Gets conjugated forms of a german verb """
+    DRIVER.get(URL.format(verb))
+
+    soup = BeautifulSoup(DRIVER.page_source, 'lxml')
+
+    verb_conj = {}
+
+    present_table = get_conjugation_table(soup, "Indicative", "Present")
+
+    """ Checking for seperable prefix """
+    if len(present_table["ich"].split(" ")) > 1:
+        verb_conj['prefix'] = present_table["ich"].split(" ")[1]
+
+    """ Parsing present tense """
+    for pronoun in present_table:
+        present_table[pronoun] = present_table[pronoun].split(" ")[0] # stripping prefix
+
+    for pronoun in ["ich", "du", "er;sie;es", "ihr", "wir"]:
+        verb_conj[pronoun] = present_table[pronoun]
+
+    """ Parsing past participle and auxillary verb"""
+
+    perfect_table = get_conjugation_table(soup, "Indicative", "Perfect")
+
+    [aux_verb, pp] = perfect_table['wir'].split(" ")
+
+    if aux_verb == "sind":
+        aux_verb = "sein"
+
+    verb_conj['aux_verb'] = aux_verb
+    verb_conj['past_participle'] = pp
+
+    """ Parsing imperfect tense """
+
+    imperfect_table = get_conjugation_table(soup, "Indicative", "Past")
+
+    verb_conj['imperfect'] = imperfect_table['ich'].split(" ")[0]
+
+    """ Parsing imperfect tense """
+
+    konjunctiv_table = get_conjugation_table(soup, "Conjunctive I and II", "Past")
+
+    verb_conj['konjunctiv'] = konjunctiv_table['ich'].split(" ")[0]
+
+    return verb_conj
+
+def get_conjugation_table(soup, voice, tense):
+    if voice == "Imperative":
+        table = soup.find("h3", text=voice) \
+                    .parent \
+                    .find("table")
+    else:
+        table = soup.find("h3", text=voice) \
+                .parent \
+                .find("h4", text=tense) \
+                .find_next_sibling("table")
+
+    conjugation_table = {}
+
+    for row in table.find_all('tr'):
+        entries = list(row.children)
+        pronoun = entries[0].text
+        conjugated_verb = entries[1].text
+        conjugation_table[pronoun] = conjugated_verb
+
+    return conjugation_table
